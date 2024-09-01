f49dc95f38c66efdb1b4106e00503e62294f7393
Junhyeok Oh <odocodani@gmail.com>
2024-09-01 23:50:18 +0900

    added some code but not working

--- TuringMachine/Main.cpp
+++ TuringMachine/Main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 #include <memory>
 
+enum class Symbol
+{
+	S0, S1,
+};
+
+enum class State
+{
+	A, B, C, HALT
+};
+
+enum class Movement
+{
+	LOWER, HIGHER,
+};
+
+struct Instruction
+{
+	State currentState;
+	Symbol inSymbol;
+	Symbol outSymbol;
+	Movement movement;
+	State nextState;
+};
+
+using Program = Instruction*;
+using Tape = Symbol*;
+using Head = size_t;
+
+struct Machine
+{
+	Tape tape = new Instruction[100];
+	Head head;
+};
+
 int main()
 {
+
 	return 0;
 }
